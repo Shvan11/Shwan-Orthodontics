2002fd8c42e741f7cb31710030bf2bc1c3a7ab6f
unknown <schwan.ortho@gmail.com>
2025-02-28 10:05:17 +0300

    test-image

---
+++ src/app/[locale]/test-image/page.tsx
@@ -0,0 +1,13 @@
+export default function TestImage() {
+    return (
+      <div style={{ padding: '20px' }}>
+        <h1>Image Test</h1>
+        <img 
+          src="/images/gallery/case2/before-1.jpg" 
+          alt="Test" 
+          width="300"
+          height="200"
+        />
+      </div>
+    );
+  }
